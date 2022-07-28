975e548d8c972d39fee178c7d25813e563fbac98
tjsharp1 <tj.tommyj@gmail.com>
2022-07-28 13:10:50 -0400

    Update t.js

--- scripts/setup-bridge-substrate/t.js
+++ scripts/setup-bridge-substrate/t.js
@@ -10,45 +10,47 @@ var alice;
 
     await api.isReady;
 
+    // NOTE: will need a 'production' bridge account for substrate
     const keyring = new Keyring({ type: 'sr25519' });
     alice = keyring.createFromUri('//Alice');
 
 
-    //await do_sudo(api, "whitelist_chain", api.tx.chainBridge.whitelistChain(1));
-    //await do_sudo(api, "add_relayer", api.tx.chainBridge.addRelayer("5CVNdoFdQcc1WggXkfEHdzEMUrnKR7jYToY3ieuoVFWAmS4C"));
-    //await do_sudo(api, "set_resource", api.tx.chainBridge.setResource("0x00000000000000000000000000000063822bbd62abfb4ab9c92210c193e71b01", "Exchange.transfer"));
-    //await do_sudo(api, "set_threshold", api.tx.chainBridge.setThreshold(1));
+    await do_sudo(api, "whitelist_chain", api.tx.chainBridge.whitelistChain(1));
+    await do_sudo(api, "add_relayer", api.tx.chainBridge.addRelayer("5CVNdoFdQcc1WggXkfEHdzEMUrnKR7jYToY3ieuoVFWAmS4C"));
+    await do_sudo(api, "set_resource", api.tx.chainBridge.setResource("0x00000000000000000000000000000063822bbd62abfb4ab9c92210c193e71b01", "Exchange.transfer"));
+    await do_sudo(api, "set_threshold", api.tx.chainBridge.setThreshold(1));
 
-    //// Fund the relayer accounts
-    //const nonce = await api.rpc.system.accountNextIndex(alice.address);
-    //const unsub = await api.tx.balances.transfer("5CVNdoFdQcc1WggXkfEHdzEMUrnKR7jYToY3ieuoVFWAmS4C", 10000000000000)
-    //    .signAndSend(alice, { nonce }, ({ status, events }) => {
-    //        if (status.isInBlock || status.isFinalized) {
-    //          events
-    //            .filter(({ event }) =>
-    //              api.events.sudo.Sudid.is(event)
-    //            )
-    //            .forEach(({ event : { data: [result] } }) => {
-    //              if (result.isError) {
-    //                let error = result.asError;
-    //                if (error.isModule) {
-    //                  const decoded = api.registry.findMetaError(error.asModule);
-    //                  const { docs, name, section } = decoded;
+    // Fund the relayer accounts
+    const nonce = await api.rpc.system.accountNextIndex(alice.address);
+    const unsub = await api.tx.balances.transfer("5CVNdoFdQcc1WggXkfEHdzEMUrnKR7jYToY3ieuoVFWAmS4C", 10000000000000)
+        .signAndSend(alice, { nonce }, ({ status, events }) => {
+            if (status.isInBlock || status.isFinalized) {
+              events
+                .filter(({ event }) =>
+                  api.events.sudo.Sudid.is(event)
+                )
+                .forEach(({ event : { data: [result] } }) => {
+                  if (result.isError) {
+                    let error = result.asError;
+                    if (error.isModule) {
+                      const decoded = api.registry.findMetaError(error.asModule);
+                      const { docs, name, section } = decoded;
 
-    //                  console.log(`${section}.${name}: ${docs.join(' ')}`);
-    //                } else {
-    //                  console.log(error.toString());
-    //                }
-    //              } else {
-    //                  console.log("No error, we're done here....");
-    //              }
-    //            });
-    //            console.log(`UNSUBBING FROM txfer`);
-    //          unsub();
-    //        }
-    //    });
+                      console.log(`${section}.${name}: ${docs.join(' ')}`);
+                    } else {
+                      console.log(error.toString());
+                    }
+                  } else {
+                      console.log("No error, we're done here....");
+                  }
+                });
+                console.log(`UNSUBBING FROM txfer`);
+              unsub();
+            }
+        });
 
-    await cb_sell(api, "2000000000000000000");
+    // EXAMPLE Substrate to ETH transaction
+    //await cb_sell(api, "2000000000000000000");
 })()
 
 
