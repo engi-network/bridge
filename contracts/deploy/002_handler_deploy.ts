7b71c12e215232118c9c2fbffe4ea4431e4cb4d4
TJ Sharp <tjsharp@TJs-MacBook-Pro-2.local>
2022-09-02 10:04:39 -0400

    Allow direct to eth exchange

--- contracts/deploy/002_handler_deploy.ts
+++ contracts/deploy/002_handler_deploy.ts
@@ -6,20 +6,19 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deploy} = deployments;
 
   const {deployer, tokenOwner} = await getNamedAccounts();
-console.log(`MY ADDER ${deployer}`);
 
-  await deploy('Vendor', {
+  const Vendor = await deployments.get("Vendor");
+
+  const handler = await deploy('ERC20Handler', {
     from: deployer,
     args: [
         // 1st arg, address of bridge contract
         '0xCEE4a38919e4BB66a7B4aBd834fd2D79563aA013',
-        // 2nd arg, address of ERC20 token
-        '0xb7D43b3c22389889A964f89F141b12D5fb1CA804',
-        // 3th resourceID
-        '0x00000000000000000000000000000063822bbd62abfb4ab9c92210c193e71b01',
+        // 4th resourceID
+        Vendor.address,
     ],
     log: true,
   });
 };
 export default func;
-func.tags = ['Vendor'];
+func.tags = ['ERC20Handler'];
